357896610d5a5effb77d12a503c402fd3a9a0891
Obasi Ngozi <ndukaubangozi@gmail.com>
2023-10-16 21:30:56 +0100

    printf

--- my-printf.c
+++ my-printf.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * _printf - Prints and handle various specifiers
- * @format: Input string with format specifier
- *
+ * _printf - Prints and handle various arrs
+ * @format: Input string with format arr
  * Return: Len.
  */
 int _printf(const char *format, ...)
 {
 	va_list my_list;
 	size_t length = 0, i = 0, j, size_of_array;
-	char newline = '\n';
 	print arr[10] = {
-	{'c', print_char}, {'s', print_str}, {'%', print_modulus}, {'d', print_int}, {'i', print_int}, {'b', print_binary}, {'u', print_unsigned}, {'o', print_octal}, {'x', print_hex_lower}, {'X', print_hex_upper},
-};
+		{'c', print_char}, {'s', print_str}, {'%', print_modulus},
+		{'d', print_int}, {'i', print_int}, {'b', print_binary},
+	};
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (0);
 	size_of_array = sizeof(arr) / sizeof(arr[0]);
@@ -27,7 +26,6 @@ int _printf(const char *format, ...)
 		else
 		{
 			i++;
-			j = 0;
 			for (j = 0; j < size_of_array; j++)
 			{
 				if (format[i] == arr[j].type_specifier)
@@ -46,8 +44,6 @@ int _printf(const char *format, ...)
 			}
 		}
 	}
-	if (format[i] == '\n')
-		write(1, &newline, 1);
 	return (length);
 	va_end(my_list);
 }
