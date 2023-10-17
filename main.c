89dca575aaa70264d95fd50ebc00da88f8456d75
Obasi Ngozi <ndukaubangozi@gmail.com>
2023-10-17 16:12:24 +0100

    task

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
+int main(void)
+{
+	int len;
+
+	len = _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+	printf("%d", len);
+	return (0);
+}
